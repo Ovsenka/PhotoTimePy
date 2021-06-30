0e5558910253bd9012998918d121f75aebd21e25
Ovsenka <ovsenka3@gmail.com>
2021-06-30 21:09:00 +0300

    Create time-image

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import vk_api
+from PIL import Image, ImageDraw, ImageFont
+import time
 
 session = vk_api.VkApi('+79869252329', 'Ovsenka200009')
 session.auth()
 
 vk = session.get_api()
+
+# Create jpg
+hour = time.strftime("%H")
+minute = time.strftime("%M")
+text = hour + ":" + minute
+
+img = Image.new('RGBA', (500, 500), 'white')
+draw = ImageDraw.Draw(img) 
+font = ImageFont.truetype("arial.ttf", size=128) # Font
+
+draw.text((100, 160), text, font=font, fill=(0, 0, 0))
+filename = "time_" + hour + "_" + minute + ".png"
+print("Saving " + filename)
+img.save(filename)
+
+
 
