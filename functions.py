a971b804cbebf6c798565973b662577f54675856
Ovsenka <ovsenka3@gmail.com>
2021-11-13 16:44:10 +0300

    Added create_time_pic()

--- functions.py
+++ functions.py
@@ -1,4 +1,32 @@
+from PIL import Image, ImageDraw, ImageFont
+import time
+# from config import path_to_save
 
-def create_time_pic():
+def create_time_pic(path_to_save=None):
+    # ---------------------------------------------------------
+    # Func that create picture with time text for profile photo
+    # ---------------------------------------------------------
+    
+    # generate time text
+    hour = time.strftime("%H")
+    minute = time.strftime("%M")
+    text = hour + ":" + minute
+    # create image
+    img = Image.new('RGBA', (500, 500), 'white')
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("arial.ttf", size=128) # Font
+    draw.text((100, 160), text, font=font, fill=(0, 0, 0))
+    
+    filename = "time_" + hour + "_" + minute + ".png"
+    print("Saving " + filename)
+    try:
+        if path_to_save != None:
+            img.save(path_to_save + filename)
+        else:
+            img.save("./Temp/" + filename)
+        print("TimeImage succesfully saved!")
+    except Exception as x:
+        print("TimeImage is not saved!")
+        print(x)
     pass
 
--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-from PIL import Image, ImageDraw, ImageFont
-import time
 from session import Session
 import functions
 
 def main():
     vk = Session()
-
-    # Create jpg
-    hour = time.strftime("%H")
-    minute = time.strftime("%M")
-    text = hour + ":" + minute
-
-    img = Image.new('RGBA', (500, 500), 'white')
-    draw = ImageDraw.Draw(img) 
-    font = ImageFont.truetype("arial.ttf", size=128) # Font
-
-    draw.text((100, 160), text, font=font, fill=(0, 0, 0))
-    filename = "time_" + hour + "_" + minute + ".png"
-    print("Saving " + filename)
-    img.save("./Temp/" + filename)
 
 if __name__ == "__main__":
     main()
