bd064889ec4c920dfa286e73615853a2660e3351
Ovsenka <ovsenka3@gmail.com>
2022-11-02 00:30:16 +0300

    img settings, import

--- dtime.py
+++ dtime.py
@@ -3,6 +3,7 @@ from time import strftime
 from exceptions import SaveImgError
 from pathlib import PurePath
 from PIL import Image, ImageDraw, ImageFont
+from settings import colors, IMG_SIZE
 
 
 def get_time() -> CurrentTime:
@@ -14,12 +15,11 @@ def create_img(time: CurrentTime) -> None:
     time_str = f"{time.hour}:{time.minute}"
     path_to_font = str(PurePath("Fonts", "Roboto-Condensed", "RobotoCondensed-Bold.ttf"))
     try:
-        
         print("[INFO] Create image...")
-        img = Image.new(mode="RGB", size=(400, 300))
+        img = Image.new(mode="RGB", size=IMG_SIZE)
         img_draw = ImageDraw.Draw(img)
         font = ImageFont.truetype(path_to_font, size=128)
-        img_draw.text((49, 80), time_str, font=font, fill=(255, 255, 255))
+        img_draw.text((49, 80), time_str, font=font, fill=colors.WHITE)
         print("[OK] time.JPEG has created!")
         print("[INFO] Saving image...")
         img.save("Temp/time", 'jpeg')
--- settings.py
+++ settings.py
@@ -5,10 +5,10 @@ from dataclasses import dataclass
 class Colors:
     BLACK: tuple[int, int, int] = (0,0,0)
     WHITE: tuple[int, int, int] = (255,255,255)
-
+    
+IMG_SIZE = (400, 300)
 
 colors = Colors()
-
 
 
 
