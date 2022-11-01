a40ac664370b041bd5d1cfdf94e2eafbf66b840d
Ovsenka <ovsenka3@gmail.com>
2022-11-01 23:15:31 +0300

    out curr time

--- dtime.py
+++ dtime.py
@@ -1,5 +1,3 @@
-from calendar import month
-from ctypes.wintypes import RGB
 from dataclasses_ import CurrentTime
 from time import strftime
 from exceptions import SaveImgError
@@ -12,12 +10,13 @@ def get_time() -> CurrentTime:
 
 from PIL import Image, ImageDraw, ImageFont
 def create_img(time: CurrentTime) -> None:
+    time_str = f"{time.hour}:{time.minute}"
     try:
         print("[INFO] Create image...")
         img = Image.new(mode="RGB", size=(400, 300))
         img_draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("Fonts//Roboto-Condensed/RobotoCondensed-Bold.ttf", size=128)
-        img_draw.text((49, 80), "11:55", font=font, fill=(255, 255, 255))
+        font = ImageFont.truetype("Fonts/Roboto-Condensed/RobotoCondensed-Bold.ttf", size=128)
+        img_draw.text((49, 80), time_str, font=font, fill=(255, 255, 255))
         print("[OK] time.JPEG has created!")
         print("[INFO] Saving image...")
         img.save("Temp/time", 'jpeg')
