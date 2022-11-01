14bf6976a444f41aabad306990c1ee096b9cbe77
Ovsenka <ovsenka3@gmail.com>
2022-11-02 00:12:29 +0300

    color settings

---
+++ settings.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Colors:
+    BLACK: tuple[int, int, int] = (0,0,0)
+    WHITE: tuple[int, int, int] = (255,255,255)
+
+
+colors = Colors()
+
+
+
+
+
+
+
