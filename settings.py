54b61743f965e9585fbae272b2203100431557e4
Ovsenka <ovsenka3@gmail.com>
2022-11-02 00:43:58 +0300

    color settings

--- settings.py
+++ settings.py
@@ -1,14 +1,25 @@
 from dataclasses import dataclass
+from pathlib import PurePath
 
 
 @dataclass
-class Colors:
+class ColorSettings:
     BLACK: tuple[int, int, int] = (0,0,0)
     WHITE: tuple[int, int, int] = (255,255,255)
-    
+
+@dataclass
+class TextSettings:
+    # Font from file
+    FONT: str = str(PurePath("Fonts", "Roboto-Condensed", "RobotoCondensed-Bold.ttf"))
+    # Size text
+    SIZE: int = 128                 
+    # Text position
+    POS: tuple[int, int] = (49, 80)
+
 IMG_SIZE = (400, 300)
 
-colors = Colors()
+colors = ColorSettings()
+text = TextSettings()
 
 
 
