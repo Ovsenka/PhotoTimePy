ef002a08fe74853b3be5c8eade2adaee850df1c6
Ovsenka <ovsenka3@gmail.com>
2022-11-03 20:58:10 +0300

    Upload exception

--- exceptions.py
+++ exceptions.py
@@ -1,2 +1,14 @@
 class SaveImgError(Exception):
     pass
+
+class UploadPhotoError(Exception):
+    def __init__(self, *args) -> None:
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+        
+    def __str__(self) -> str:
+        if self.message:
+            return f"{self.message}"
+        return "UploadPhotoError has been raised!"
